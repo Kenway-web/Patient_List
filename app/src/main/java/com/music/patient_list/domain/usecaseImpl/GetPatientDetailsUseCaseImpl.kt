24c46acf4b85d9e6f6346812fe9d45b609e743f7
Shobhit <shobhitsingh1149201@gmail.com>
2024-07-17 11:55:08 +0530

    PatientDetailById using UseCasees

--- app/src/main/java/com/music/patient_list/domain/usecaseImpl/GetPatientDetailsUseCaseImpl.kt
+++ app/src/main/java/com/music/patient_list/domain/usecaseImpl/GetPatientDetailsUseCaseImpl.kt
@@ -1,12 +1,16 @@
 package com.music.patient_list.domain.usecaseImpl
 
+import com.music.patient_list.domain.model.PatientDetailsEntity
 import com.music.patient_list.domain.repo.PatientRepo
 import com.music.patient_list.domain.usecase.GetPatientDetailUseCase
 import com.music.patient_list.presentation.PatientDetails.PatientDetailsEvent
 import javax.inject.Inject
 
-class GetPatientDetailsUseCaseImpl @Inject constructor(private val patientRepo: PatientRepo) : GetPatientDetailUseCase {
-    override suspend fun getPatientDetailList(): PatientDetailsEvent {
-        TODO("Not yet implemented")
+class GetPatientDetailsUseCaseImpl @Inject constructor(
+    private val patientRepo: PatientRepo
+) : GetPatientDetailUseCase {
+
+    override suspend fun getPatientDetailById(id: Int): PatientDetailsEntity? {
+        return patientRepo.getPatientById(id)
     }
 }
